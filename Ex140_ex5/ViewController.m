1164453d8dbe05e4734dbdae217ffd2c0a2f867c
saerem21 <nemessisdds>
2014-01-06 14:18:57 +0900

    타이머 카운트 입니다..
    140page

--- Ex140_ex5/ViewController.m
+++ Ex140_ex5/ViewController.m
@@ -9,10 +9,47 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
 
 @end
 
 @implementation ViewController
+{
+    NSTimer *timer;
+    BOOL timerWorking;
+    
+}
+
+- (void)tickDown:(NSTimer *)timer{
+    self.datePicker.countDownDuration = self.datePicker.countDownDuration -60;
+    
+    if(self.datePicker.countDownDuration <= 60 ){
+        NSLog(@"done!");
+        [self stopTimer];
+    }
+}
+
+- (void)startTimer{
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(tickDown:) userInfo:Nil repeats:YES];
+}
+- (void)stopTimer{
+    [timer invalidate];
+    timer = Nil;
+    timerWorking = NO;
+}
+
+- (IBAction)toggleBtn:(id)sender {
+    NSString *title = timerWorking ? @"statr" : @"stop";
+    [sender setTitle:title forState:UIControlStateNormal];
+    timerWorking = !timerWorking;
+    
+    if(timerWorking){
+        [self startTimer];
+    }
+    else{
+        [self stopTimer];
+    }
+}
 
 - (void)viewDidLoad
 {
